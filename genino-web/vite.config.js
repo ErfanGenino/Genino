82767898793ac8f3919cf238d82cca6c6680d52a
Erfan Samavati <samavati.erfan@gmail.com>
2025-10-14 14:59:51 +0330

    Fix: correct base path for Vercel deployment

--- genino-web/vite.config.js
+++ genino-web/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: "/Genino/", // 👈 مسیر دقیق برای GitHub Pages
 });
